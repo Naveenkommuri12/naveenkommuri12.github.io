76a359ef5ab8953d42a062f8c26c312c377cb432
Naveen <naveenkommuri22@gmail.com>
2023-09-21 00:50:14 -0400

    Updated

---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+export { default as AsurionLogo } from "./organizations/webp/asurion-logo.webp";
+export { default as ChannelSoftLogo } from "./organizations/webp/channel-soft-logo.webp";
+
+// Personal Assets
+export { default as ProfessionalHeadshot } from "./personal/webp/professional-headshot.webp";
+export { default as FallbackProfessionalHeadshot } from "./personal/jpg/professional-headshot.jpg";
+export { default as HomeBackground } from "./personal/webp/background.webp";
+export { default as FallbackHomeBackground } from "./personal/jpg/background.jpg";
